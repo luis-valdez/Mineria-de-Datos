660779e7f48e1ceba246f1c0409c18385f6f2681
luiscvr <luiscarlosvaldezr@gmail.com>
2019-03-18 18:10:31 -0700

    Se agregaron lineas para probar efectividad(TEST)

--- Bayes_Cancer/EncontrarFrecuencias.m
+++ Bayes_Cancer/EncontrarFrecuencias.m
@@ -27,4 +27,40 @@ end
 freq_benignos = freq_benignos + 1;
 freq_malignos = freq_malignos + 1;
 
+%Sacar probabilidades
+
+prob_benignos = zeros(size(freq_benignos));
+prob_malignos = zeros(size(freq_malignos));
+num_atributos = size(prob_benignos,1);
+num_valores = size(prob_benignos,2);
+for i = 1 : num_atributos
+    for j = 1 : num_valores
+        prob_benignos(i,j) =  freq_benignos(i,j) / sum(freq_benignos(i,:));
+        prob_malignos(i,j) =  freq_malignos(i,j) / sum(freq_malignos(i,:));
+    end
+end
+
+%Probar el algoritmo
+Test_Puro = Test(1:239,2:10);
+%Sacar transpuestas para mejor organizacion
+prob_benignos = transpose(prob_benignos);
+prob_malignos = transpose(prob_malignos);
+%Vector que nos dira si cada caso del test es benigno(2) o maligno(4)
+Asignaciones = zeros(size(Test_Puro,1),1);
+for i = 1 : size(Test_Puro,1)
+    prob_ser_benigno=prob_apriori_ben;
+    prob_ser_maligno=prob_apriori_mal;
+    for j = 1 : size(Test_Puro,2)
+        prob_ser_benigno = prob_ser_benigno * prob_benignos(Test_Puro(i,j) , j);
+        prob_ser_maligno = prob_ser_maligno * prob_malignos(Test_Puro(i,j) , j);
+        
+    end
+    if(prob_ser_benigno < prob_ser_maligno)
+        Asignaciones(i) = 4;
+    else
+        Asignaciones(i) = 2;
+    end
+end
+
+porcentajeSimilitud(Asignaciones, Test(:,11))
 
